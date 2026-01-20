0ec9864fb7bd1519e0b44b7ae1569153ef9c2cff
José Torres <81926348+jctorrestone@users.noreply.github.com>
2026-01-20 11:10:11 -0500

    Refactor

---
+++ about.js
@@ -0,0 +1,7 @@
+class About extends HTMLElement {
+    constructor() {
+        super();
+    }
+}
+
+customElements.define("about-page", About);
---
+++ projects.js
@@ -0,0 +1,7 @@
+class Projects extends HTMLElement {
+    constructor() {
+        super();
+    }
+}
+
+customElements.define("projects-page", Projects);
